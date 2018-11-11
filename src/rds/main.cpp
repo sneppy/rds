2a005429cf17bd42584829f533cc4b791df3969e
sneppy <andrea.mecchia@mail.polimi.it>
2018-11-12 00:40:01 +0100

    Full specialization of Vec3<float32>
    * Fixed missing return statement in compound vector-vector operations

--- src/rds/main.cpp
+++ src/rds/main.cpp
@@ -2,10 +2,12 @@
 
 int main()
 {
-	Vec3<float32> v1(1.f, 2.f, 3.f);
-	Vec3<int32> v2(1, 2, 3);
-	v1 = v1.normalize() + v2;
-	printf("%f\n", v1.y);
-	printf("%f\n", v2.getSquaredSize());
+	Vec3<> v1(1.f, 0.f, 0.f), v2(0.f, 1.f, 0.f), v3, v4;
+	Vec3<int32> v5(1, 0, 0), v6(0, 1, 0), v7, v8;
+	v3 = v1 ^ v2;
+	v7 = v5 ^ v6;
+	v1.print();
+	v2.print();
+	v3.print();
 	return 0;
 }
