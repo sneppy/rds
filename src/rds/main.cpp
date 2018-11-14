8a7b8098a211d88b6c775416665b7e21eb0b936b
sneppy <andrea.mecchia@mail.polimi.it>
2018-11-14 13:55:11 +0100

    Minor improvements in Vec structs
    * Comparison now uses FLT_EPSILON as tolerance

--- src/rds/main.cpp
+++ src/rds/main.cpp
@@ -1,9 +1,23 @@
-#include "math/quat.h"
+#include "math/mat4.h"
 
 int main()
 {
-	Quat q1(vec3(0.f, 1.f, 0.f), 3.14f / 2.f), q2(vec3(0.f, 1.f, 0.f), 6.28f);
-	(q1 * vec3::backward).print();
-	q1.toForward().print();
+	vec3 v(1.f), r(2.f);
+	vec2 v2(1.f);
+	printf("%f\n", v & r);
+	mat4 m(
+		2.f, 1.f, 0.f, 1.f,
+		3.f, 1.f, 1.f, 1.f,
+		2.f, 0.f, 3.f, 1.f,
+		0.f, 0.f, 0.f, 1.f
+	);
+	//m = mat4::translation(vec3(4.f));
+	m.print();
+	(!m).print();
+	auto start = clock();
+	for (uint64 i = 0; i < 1e5; ++i)
+		//m.getDeterminant();
+		v2.getNormal();
+	printf("%f\n", float(clock() - start) / CLOCKS_PER_SEC);
 	return 0;
 }
